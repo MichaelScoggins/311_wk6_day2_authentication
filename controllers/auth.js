37f05b27d92a1095750217b96323543ac67de55b
michaelscoggins <rhythmghosts@gmail.com>
2020-09-12 00:09:52 -0500

    removed scopeID but i dont think its working still

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const axios = require('axios')
+const dotenv = require('dotenv')
+dotenv.config()
 const mysql = require('mysql')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
@@ -27,19 +29,17 @@ const saltRounds = 10
 
 const login = (req, res) => {
   const { username, password } = req.body
-
   axios(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
     method: 'POST',
     headers: {
       'content-type': 'application/json'
     },
     data: {
-      grant_type: 'password',
+      grant_type: 'password',      
+      connection: 'Username-Password-Authentication',
       username: username,
       password: password,
-      audience: process.env.AUTH0_IDENTITY,
-      connection: 'Username-Password-Authentication',
-      scope: 'openid',
+      audience: process.env.AUTH0_DOMAIN,
       client_id: process.env.AUTH0_CLIENT_ID,
       client_secret: process.env.AUTH0_CLIENT_SECRET
     }
